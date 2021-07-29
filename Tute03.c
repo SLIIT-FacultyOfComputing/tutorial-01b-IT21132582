23945187442ee35c29759b082ca268fad5c34e38
it21132582 <87386265+IT21132582@users.noreply.github.com>
2021-07-29 11:00:35 +0000

    Tutorial 1 q 3

--- Tute03.c
+++ Tute03.c
@@ -13,6 +13,17 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+  int n, sum=0, i;
+  printf("Enter no. to get sum:");
+  scanf("%d", &n);
+
+  for(i=0;i<=n; ++i){
+    sum+=i;
+  }
+
+  printf("The sum is: %d", sum);
+
+  
   
   return 0;
 }
